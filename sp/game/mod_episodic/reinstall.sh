005feb5e1af5249ecfbdc1a8c7db3c49f0c75b0b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-03-29 19:38:09 -0700

    add wepon sound lol

--- sp/game/mod_episodic/reinstall.sh
+++ sp/game/mod_episodic/reinstall.sh
@@ -8,11 +8,20 @@ fi
 ln -sf ${PWD}/bin/client.so ${SOTES_INSTALL_PATH}/bin/
 ln -sf ${PWD}/bin/server.so ${SOTES_INSTALL_PATH}/bin/
 
-rm ${SOTES_INSTALL_PATH}/cfg/skill.cfg
+# rm ${SOTES_INSTALL_PATH}/gameinfo.txt
+cp ./gameinfo.txt ${SOTES_INSTALL_PATH}/gameinfo.txt
+
+# rm ${SOTES_INSTALL_PATH}/cfg/skill.cfg
 cp ./cfg/skill.cfg ${SOTES_INSTALL_PATH}/cfg/skill.cfg
 
-rm ${SOTES_INSTALL_PATH}/scripts/weapon_mp5.txt
+mkdir -p ${SOTES_INSTALL_PATH}/sound/weapons/dl44
+cp ./sound/weapons/dl44/dl44_fire1.wav ${SOTES_INSTALL_PATH}/sound/weapons/dl44/dl44_fire1.wav
+
+# rm ${SOTES_INSTALL_PATH}/scripts/game_sounds_weapons.txt
+cp ./scripts/game_sounds_weapons.txt ${SOTES_INSTALL_PATH}/scripts/game_sounds_weapons.txt
+
+# rm ${SOTES_INSTALL_PATH}/scripts/weapon_mp5.txt
 cp ./scripts/weapon_mp5.txt ${SOTES_INSTALL_PATH}/scripts/weapon_mp5.txt
 
-rm ${SOTES_INSTALL_PATH}/scripts/weapon_dl44.txt
+# rm ${SOTES_INSTALL_PATH}/scripts/weapon_dl44.txt
 cp ./scripts/weapon_dl44.txt ${SOTES_INSTALL_PATH}/scripts/weapon_dl44.txt
